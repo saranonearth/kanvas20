8364ba725e45a911a6780595b6b6f135b63cfb88
saranonearth <saran.suresh@outlook.com>
2020-04-12 02:59:51 +0530

    Added: Event Details

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ var rellax = new Rellax('.rellax', {
     horizontal: false,
     breakpoints: [576, 768, 1201]
 });
+
+//conditional rendering event data
+
+const wd_button = document.querySelector('.wd')
+const main = document.querySelector('.flex');
+const WD = document.querySelector('.flex-1');
+const GD = document.querySelector('.flex-2');
+const gd_button = document.querySelector('.gd');
+const back1_button = document.querySelector('#back1');
+const back2_button = document.querySelector('#back2');
+
+wd_button.addEventListener('click', () => {
+    main.style.display = 'none';
+    WD.style.display = 'block';
+})
+
+gd_button.addEventListener('click', () => {
+    main.style.display = 'none';
+    GD.style.display = 'block';
+    window.scrollTo(950, 950);
+})
+
+back1_button.addEventListener('click', () => {
+    back()
+})
+back2_button.addEventListener('click', () => {
+    back()
+})
+
+const back = () => {
+    main.style.display = 'flex';
+    WD.style.display = 'none';
+    GD.style.display = 'none';
+}
