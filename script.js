5015ef65d2a37049b2e5050b65ef90ced4fb0e15
saranonearth <saran.suresh@outlook.com>
2020-04-12 01:38:04 +0530

    Added: Parallax

---
+++ script.js
@@ -0,0 +1,11 @@
+const Rellax = window.Rellax
+
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: true,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+    breakpoints: [576, 768, 1201]
+});
