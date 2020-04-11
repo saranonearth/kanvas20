0d70a0b44e272f9f7b7a6d6231397277105542dd
saranonearth <saran.suresh@outlook.com>
2020-04-12 03:43:09 +0530

    Added: Submit form

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ const wd_button = document.querySelector('.wd')
 const main = document.querySelector('.flex');
 const WD = document.querySelector('.flex-1');
 const GD = document.querySelector('.flex-2');
+const SUB = document.querySelector('.flex-3');
 const gd_button = document.querySelector('.gd');
-const back1_button = document.querySelector('#back1');
-const back2_button = document.querySelector('#back2');
-
+const SUB_button = document.querySelector('.org-button');
+const events_heading = document.querySelector('.contract-title');
 wd_button.addEventListener('click', () => {
     main.style.display = 'none';
     WD.style.display = 'block';
@@ -31,15 +31,24 @@ gd_button.addEventListener('click', () => {
     window.scrollTo(950, 950);
 })
 
-back1_button.addEventListener('click', () => {
-    back()
-})
-back2_button.addEventListener('click', () => {
-    back()
+SUB_button.addEventListener('click', () => {
+    console.log(events_heading)
+    main.style.display = 'none';
+    GD.style.display = 'none';
+    WD.style.display = 'none';
+    SUB.style.display = 'block';
+    events_heading.style.display = 'none'
+    if (window.screen.width * window.devicePixelRatio < 650) {
+        window.scrollTo(600, 600);
+    } else {
+        window.scrollTo(950, 950);
+    }
 })
 
 const back = () => {
     main.style.display = 'flex';
     WD.style.display = 'none';
+    events_heading.display = 'block'
     GD.style.display = 'none';
+    SUB.style.display = 'none';
 }
